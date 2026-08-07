e86f4791ccdbfe64d45f93944f7dba0e65a56906
Berken Sayilir <sayilir.berken@gmail.com>
2026-08-07 10:36:37 +0200

    Apply Swiftly detection style pass

--- Sources/SwiftlyKit/Extensions/PlatformOptions.swift
+++ Sources/SwiftlyKit/Extensions/PlatformOptions.swift
@@ -5,9 +5,7 @@ extension PlatformOptions {
     static var swiftlyKitProcess: PlatformOptions {
         var options = PlatformOptions()
         options.createSession = true
-        options.teardownSequence = [
-            .gracefulShutDown(toProcessGroup: true, allowedDurationToNextStep: .seconds(1))
-        ]
+        options.teardownSequence = [.gracefulShutDown(toProcessGroup: true, allowedDurationToNextStep: .seconds(1))]
         return options
     }
     
